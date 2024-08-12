a5d0a61f7a9d8ab5635140ad51220d842a18624f
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-08-12 08:30:38 +0200

    configure the student service

--- microservices/student/src/main/java/ksike/sw/student/StudentApplication.java
+++ microservices/student/src/main/java/ksike/sw/student/StudentApplication.java
@@ -2,7 +2,9 @@ package ksike.sw.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class StudentApplication {
 
