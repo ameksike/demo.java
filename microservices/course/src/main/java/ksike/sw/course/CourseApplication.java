14ca3312d4d10e5bb7b02dc791df71ed340041a0
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-08-12 08:34:06 +0200

    add subproject course

---
+++ microservices/course/src/main/java/ksike/sw/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package ksike.sw.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
